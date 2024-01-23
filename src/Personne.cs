410e4ed6704746d5242df6fb3241f89af518d3e4
SebastienAuclere <137295360+SebastienAuclere@users.noreply.github.com>
2024-01-23 16:41:27 +0100

    Class Personne

---
+++ src/Personne.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class Personne
+    {
+        private string nom;
+        private string prenom;
+
+        public Personne(string unNom, string unPrenom)
+        {
+           this.nom = unNom;
+           this.prenom = unPrenom;  
+        }
+
+        public string Infos()
+        {
+            return nom + " " + prenom " ";
+        }
+
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne unePersonne;
+            unePersonne =  new  Personne("Alibert", "Louis"); 
+            Console.WriteLine(unePersonne.Infos());      
+        }
+    } 
+}
